e37b555500e65ba11ca6b802752b635ca0e47a97
Samuel-Dare <oluwadaresamuel39@gmail.com>
2022-06-07 12:22:40 +0100

    djdf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Stories from "./Stories";
+import Footer from "./Footer";
+import CreateStory from "./CreateStory";
+import UserWelcome from "./UserWelcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="stories" element={<Stories />} />
+        <Route path="createstory" element={<CreateStory />} />
+        <Route path="user" element={<UserWelcome />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/Stories.js
@@ -0,0 +1,217 @@
+import React from "react";
+import "./Stories.css";
+
+const Stories = () => {
+  return (
+    <div>
+      <section className="you-have-a-story d-lg-flex justify-content-between">
+        <div>
+          <h2>
+            You've got a story, Post<span>it</span>.
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+            sapiente temporibus ducimus inventore laborum, laudantium tenetur
+            nihil facere consequuntur mollitia saepe nulla doloribus minima in
+            accusamus iste tempore reprehenderit cumque?
+          </p>
+          <input type="text" placeholder="Email address" />
+          <button>Get Started</button>
+        </div>
+        <div>
+          <img src="./Images/unsplash_Z2bJeJQRbW0.png" alt="" />
+        </div>
+      </section>
+      <section className="stories container row">
+        <section className="story-one col-lg-3">
+          <img src="./Images/Rectangle 42-8.png" alt="" />
+          <h6></h6>
+          <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+          <pre>
+            <img src="" /> By Maria Davies - June 5, 2022
+          </pre>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+            sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+            recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+            quidem cupiditate consectetur perferendis.
+          </p>
+          <a href="">Read More...</a>
+        </section>
+        <section className="story-one col-lg-3">
+          <img src="./Images/Rectangle 42-8.png" alt="" />
+          <h6></h6>
+          <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+          <pre>
+            <img src="" /> By Maria Davies - June 5, 2022
+          </pre>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+            sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+            recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+            quidem cupiditate consectetur perferendis.
+          </p>
+          <a href="">Read More...</a>
+        </section>
+        <section className="story-one col-lg-3">
+          <img src="./Images/Rectangle 42-8.png" alt="" />
+          <h6></h6>
+          <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+          <pre>
+            <img src="" /> By Maria Davies - June 5, 2022
+          </pre>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+            sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+            recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+            quidem cupiditate consectetur perferendis.
+          </p>
+          <a href="">Read More...</a>
+        </section>
+        <section className="stories row">
+          <section className="story-one col-lg-3">
+            <img src="./Images/Rectangle 42-8.png" alt="" />
+            <h6></h6>
+            <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+            <pre>
+              <img src="" /> By Maria Davies - June 5, 2022
+            </pre>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+              sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+              recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+              quidem cupiditate consectetur perferendis.
+            </p>
+            <a href="">Read More...</a>
+          </section>
+          <section className="story-one col-lg-3">
+            <img src="./Images/Rectangle 42-8.png" alt="" />
+            <h6></h6>
+            <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+            <pre>
+              <img src="" /> By Maria Davies - June 5, 2022
+            </pre>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+              sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+              recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+              quidem cupiditate consectetur perferendis.
+            </p>
+            <a href="">Read More...</a>
+          </section>
+          <section className="story-one col-lg-3">
+            <img src="./Images/Rectangle 42-8.png" alt="" />
+            <h6></h6>
+            <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+            <pre>
+              <img src="" /> By Maria Davies - June 5, 2022
+            </pre>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+              sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+              recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+              quidem cupiditate consectetur perferendis.
+            </p>
+            <a href="">Read More...</a>
+          </section>
+        </section>
+        <section className="stories row">
+          <section className="story-one col-lg-3">
+            <img src="./Images/Rectangle 42-8.png" alt="" />
+            <h6></h6>
+            <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+            <pre>
+              <img src="" /> By Maria Davies - June 5, 2022
+            </pre>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+              sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+              recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+              quidem cupiditate consectetur perferendis.
+            </p>
+            <a href="">Read More...</a>
+          </section>
+          <section className="story-one col-lg-3">
+            <img src="./Images/Rectangle 42-8.png" alt="" />
+            <h6></h6>
+            <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+            <pre>
+              <img src="" /> By Maria Davies - June 5, 2022
+            </pre>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+              sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+              recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+              quidem cupiditate consectetur perferendis.
+            </p>
+            <a href="">Read More...</a>
+          </section>
+          <section className="story-one col-lg-3">
+            <img src="./Images/Rectangle 42-8.png" alt="" />
+            <h6></h6>
+            <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+            <pre>
+              <img src="" /> By Maria Davies - June 5, 2022
+            </pre>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+              sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+              recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+              quidem cupiditate consectetur perferendis.
+            </p>
+            <a href="">Read More...</a>
+          </section>
+        </section>
+        <section className="stories row">
+          <section className="story-one col-lg-3">
+            <img src="./Images/Rectangle 42-8.png" alt="" />
+            <h6></h6>
+            <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+            <pre>
+              <img src="" /> By Maria Davies - June 5, 2022
+            </pre>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+              sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+              recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+              quidem cupiditate consectetur perferendis.
+            </p>
+            <a href="">Read More...</a>
+          </section>
+          <section className="story-one col-lg-3">
+            <img src="./Images/Rectangle 42-8.png" alt="" />
+            <h6></h6>
+            <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+            <pre>
+              <img src="" /> By Maria Davies - June 5, 2022
+            </pre>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+              sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+              recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+              quidem cupiditate consectetur perferendis.
+            </p>
+            <a href="">Read More...</a>
+          </section>
+          <section className="story-one col-lg-3">
+            <img src="./Images/Rectangle 42-8.png" alt="" />
+            <h6></h6>
+            <h5>The 20 Biggest Fintech Companies in Nigeria 2022</h5>
+            <pre>
+              <img src="" /> By Maria Davies - June 5, 2022
+            </pre>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+              sequi doloribus ipsam. Asperiores iste, minus alias consequuntur
+              recusandae, eveniet ab nemo officia ipsam neque beatae distinctio
+              quidem cupiditate consectetur perferendis.
+            </p>
+            <a href="">Read More...</a>
+          </section>
+        </section>
+      </section>
+    </div>
+  );
+};
+
+export default Stories;
